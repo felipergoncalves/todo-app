56fab68467e92b4094065e2c9d543ddbf6be93f5
Felipe <feferodrigues771@gmail.com>
2023-04-16 22:40:16 -0300

    Creating Add Task Form

---
+++ src/components/CustomForm.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import { PlusIcon } from '@heroicons/react/24/solid'
+
+const CustomForm = ({ addTask }) => {
+    const [task, setTask] = useState("")
+
+    const handleFormSubmit = (e) => {
+        e.preventDefault();
+        addTask({
+            name: task,
+            checked: false,
+            id: Date.now()
+        })
+        setTask("")
+    }
+
+    return(
+        <form className="todo" onSubmit={handleFormSubmit}>
+            <div className="wrapper">
+                <input 
+                    type="text"
+                    id="task"
+                    className="input"
+                    value={task}
+                    onInput={(e) => setTask(e.target.value)}
+                    required
+                    autoFocus
+                    maxLength={60}
+                    placeholder="Enter Task"
+                />
+                <label htmlFor="task" className="label">Enter Task</label>
+                <button
+                    className="btn"
+                    aria-label="Add Task"
+                    type="submit"
+                >
+                  <PlusIcon/>
+                </button>
+            </div>
+        </form>
+    )
+}
+
+export default CustomForm
